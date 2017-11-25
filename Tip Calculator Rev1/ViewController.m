852b9835bd980e37e790f3dad6ea46c4f961998c
jos3barros <barros_j@yahoo.com>
2017-11-25 11:02:11 -0200

    Original version Rev -0

--- Tip Calculator Rev1/ViewController.m
+++ Tip Calculator Rev1/ViewController.m
@@ -8,7 +8,11 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UITextField *billAmount;
+@property (weak, nonatomic) IBOutlet UITextField *percent;
+@property (weak, nonatomic) IBOutlet UILabel *display;
 
 @end
 
@@ -16,9 +20,30 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    self.billAmount.delegate = self;
+    self.percent.delegate = self;
+    UITapGestureRecognizer* tapGesture = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(dismissKeyboard)];
+    [self.view addGestureRecognizer:tapGesture];
 }
 
+- (void) dismissKeyboard {
+    // this method will resign first responder or end editting, and dismiss the keyboard
+    [self.billAmount resignFirstResponder];
+    [self.percent resignFirstResponder];
+    //[self.view endEditing:YES]; alternate way of dismissing keyboard
+}
+
+- (IBAction)calculateTip:(UIButton *)sender {
+    [self.view endEditing:YES]; // alternate way implemented here
+    float billAmountFloat = [self.billAmount.text floatValue];
+    float percentFloat = [self.percent.text floatValue];
+    float tipAmount = billAmountFloat * percentFloat/100;
+    self.display.text = [NSString stringWithFormat:@"$%.2f", tipAmount];
+}
+
+- (IBAction)sliderValueChanged:(UISlider*)sender {
+    self.percent.text = [NSString stringWithFormat:@"%.0f",sender.value*100];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
